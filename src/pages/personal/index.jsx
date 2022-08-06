caa6a7d79208dc55326d270caac5e2cb70fc16c6
LXJ <526392973@qq.com>
2022-08-06 11:18:32 +0800

    微信登录

--- src/pages/personal/index.jsx
+++ src/pages/personal/index.jsx
@@ -1,10 +1,18 @@
 import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
+import {View, Text, Button} from '@tarojs/components'
 import TabBar from "../common/tabBar";
 
 
 
 class Personal extends Component {
+    constructor(args) {
+        super(args);
+        this.state={
+            sessionKey:"ss",
+            openId:"op"
+        }
+    }
+
     componentWillReceiveProps (nextProps) {
         console.log(this.props, nextProps)
     }
@@ -16,9 +24,109 @@ class Personal extends Component {
     componentDidHide () { }
 
     render () {
+        var that = this;
+
+        function getUserProfile1(){
+            console.log(that.state.sessionKey);
+            console.log("1111");
+            wx.getUserProfile({
+                desc: '展示用户信息',
+                success: (res1) =>{
+                    //res1.signature = babcb0b9e91f5b329d3a3c68e2653bf6fbca35ca
+                    // signature: res1.signature
+                    // rawData: res1.rawData
+
+                    console.log("res1",res1)
+                    // getInfo(res.userInfo, res)
+
+
+                    // var url = 'https://g1.glypro19.com/wx/login'
+                    var url = 'http://localhost:8090/wx/login'
+                    wx.showLoading({title:"登录中", mask:"true"})
+
+                    wx.login({
+                        success (res2){
+                            // res2中有code
+                            console.log("login成功,res2",res2);
+                            console.log("code" , res2.code)
+                            console.log("rawData" , res1.rawData)
+                            console.log("signature" , res1.signature)
+
+                            if (res2.code){
+                                wx.request({
+                                    url: url,
+                                    data: {
+                                        code: res2.code,
+                                        rawData: res1.rawData,
+                                        signature: res1.signature
+                                    },
+                                    method:"POST",
+
+                                    success: (res3)=>{
+                                        console.log("res3",res3);
+                                        console.log(res3.data);
+                                        that.state.sessionKey = res3.data.data.sessionKey;
+                                        that.state.openId = res3.data.data.openId;
+                                        if (res3.data.code === 200){
+                                            let userInfo = Object.assign(res1, res3.data)
+                                            console.log("登录成功！")
+                                            console.log(userInfo);
+                                            //userInfo 里有 {encryptedData, iv...}
+
+                                            wx.hideLoading();
+                                            wx.stopPullDownRefresh();
+                                        }else{
+                                            wx.showToast({title: "登录失败！",icon:"error",duration:2000})
+                                            wx.hideLoading()
+                                            wx.stopPullDownRefresh()
+                                        }
+                                    }
+                                })
+                            }else{
+                                console.log('登录失败！' + res2.errMsg)
+                            }
+                        }
+
+                    })
+                },
+                fail: (err) => {
+                    console.log(err)
+                }
+            })
+        }
+
+
+        function getPhoneNumber(e) {
+            console.log("手机")
+            console.log(e)
+            console.log(e.detail.errMsg == "getPhoneNumber:ok");
+            if (e.detail.errMsg == "getPhoneNumber:ok") {
+                wx.request({
+                    url: 'http://localhost:8090/wx/getPhoneNumber',
+                    data: {
+                        encryptedData: e.detail.encryptedData,
+                        iv: e.detail.iv,
+                        sessionKey: that.state.sessionKey,
+                        openId:that.state.openId
+                    },
+                    method: "post",
+                    success: function (res) {
+                        console.log(res);
+                    }
+                })
+            }
+            ;
+        }
+
+        function bindgetphonenumber(e) {
+            console.log("bind");
+        }
+
         return (
             <View>
                 <View><Text>个人信息展示</Text></View>
+                <Button onClick={getUserProfile1}>测试</Button>
+                <Button openType={"getPhoneNumber"} onGetPhoneNumber={getPhoneNumber} >绑定手机</Button>
                 <TabBar tabBarCurrent={3} />
             </View>
         )
@@ -26,4 +134,3 @@ class Personal extends Component {
 }
 
 export default Personal
-
